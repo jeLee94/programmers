9c84325709f81cb26e2e0c623dc6592bf086e88a
jelee <88365786+jeLee94@users.noreply.github.com>
2022-11-11 16:28:22 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,16 @@
+function solution(price) {
+    let result = 0
+    if(price >= 500000){
+        result = price*0.8
+    }
+    else if(price>=300000){
+        result = price*0.9
+    }
+    else if(price>=100000){
+        result = price*0.95
+    }
+    else result=price
+    
+    return parseInt(result)
+}
+
