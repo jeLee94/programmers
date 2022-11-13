bb139643da06431971e89b43dca9d0404382e401
jelee <88365786+jeLee94@users.noreply.github.com>
2022-11-13 23:28:38 +0900

    [level 0] Title: 모스부호 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    let result = ''
+    let letters = letter.split(' ')
+    for (let i =0;i<letters.length;i++){
+        result += morse[letters[i]]
+    }
+    return result
+}
