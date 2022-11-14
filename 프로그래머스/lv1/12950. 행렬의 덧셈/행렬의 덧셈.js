62766f5a2a101ffa0037f57dbb1cb706a9756fe9
jelee <88365786+jeLee94@users.noreply.github.com>
2022-11-14 14:33:42 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.19 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,3 @@
 function solution(arr1, arr2) {
-    let result = Array.from(Array(arr1.length), () => Array(arr1[0].length));
-
-    for (i in arr1) {
-      for (j in arr1[i]) {
-        result[i][j] = arr1[i][j] + arr2[i][j];
-        
-      }
-    }
-    return result
+    return arr1.map((a,i)=> a.map((b,j)=> b + arr2[i][j]))
 }
