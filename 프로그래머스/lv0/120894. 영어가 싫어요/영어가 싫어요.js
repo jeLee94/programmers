bdbdc6028e77440481c7d5fce52bec3171948d08
jelee <88365786+jeLee94@users.noreply.github.com>
2022-12-12 13:34:17 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,9 +2,7 @@ function solution(numbers) {
     var engs = ['zero','one','two','three','four','five','six','seven','eight','nine']
     
     for(let idx in engs){
-        if(numbers.includes(engs[idx])){
-            numbers = numbers.replaceAll(engs[idx],idx)
-        }
+        numbers = numbers.replaceAll(engs[idx],idx)
     }
     
     return Number(numbers);
