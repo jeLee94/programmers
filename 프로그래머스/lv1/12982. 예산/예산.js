1f6bf364f19c248ad03b83197f0143347737f243
JungEun LEE <88365786+jeLee94@users.noreply.github.com>
2023-03-12 21:11:48 +0900

    [level 1] Title: 예산, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,13 +1,7 @@
 function solution(d, budget) {
-    let departments = d.sort((a,b)=>a-b)
-    let count = 0
-    let sum = 0
-    for(let i=0; i<departments.length; i++){
-        sum += departments[i]        
-        if(sum>budget){
-            return count
-        }else{count++}
-    }
-    return count
-    
+    d.sort((a, b) => a - b);
+
+    while (d.reduce((a, b) => (a + b), 0) > budget) d.pop();
+
+    return d.length;
 }
