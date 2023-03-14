14ce49e54e21366a2ea8033853403c31abb9d5dd
JungEun LEE <88365786+jeLee94@users.noreply.github.com>
2023-03-14 21:24:52 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,10 @@
+function solution(id_pw, db) {
+    for(const item of db){
+        if(item[0] === id_pw[0]){
+            if(item[1] === id_pw[1]){
+                return "login"
+            }else{return "wrong pw"}
+        }
+    }
+    return "fail"
+}
