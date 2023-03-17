2b5d8cbc48673a55b4afac62a6f3bbfb8098081b
JungEun LEE <88365786+jeLee94@users.noreply.github.com>
2023-03-17 15:15:12 +0900

    [level 1] Title: 폰켓몬, Time: 0.42 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,3 @@
 function solution(nums) {
-    const set = new Set(nums)
-    return nums.length/2 < Array.from(set).length ? nums.length/2 : Array.from(set).length
+    return nums.length/2 < new Set(nums).size ? nums.length/2 : new Set(nums).size
 }
